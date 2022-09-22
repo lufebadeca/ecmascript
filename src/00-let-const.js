ec6db7c6c3e980d7a260fe0fd922a49a32fc0119
unknown <lufebadeca@gmail.com>
2022-09-22 13:24:22 -0500

    ecmascript let,var, const, arrow practice

---
+++ src/00-let-const.js
@@ -0,0 +1,17 @@
+var lastName = 'David';
+lastName = 'Oscar';
+console.log(lastName);
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'cat';
+console.log(animal);
+
+const fruits = () =>{
+    if (){
+        var fruit1 = 'Apple';
+    }
+}
